91076e0c806810e10df518ecfbe1520279a121e7
Mounika <mounikasaiaarepu09@gmail.com>
2024-11-25 12:49:29 +0530

    variable-simple

---
+++ 02.variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Avika:: Hello Monika, How are you?"
+echo "Monika:: Hi Avika, I am fine. How are you?"
+echo "Avika: I am fine too. how is your work?"
+echo "Monika:: not bad. I am thinking to upgrade to DevOps"
